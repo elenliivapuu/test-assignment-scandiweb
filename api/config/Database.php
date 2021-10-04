26c5e3d1eb1b7b175a7a88b6f834e8ef136fc66e
Elen Liivapuu <eliivapuu@gmail.com>
2021-10-04 18:47:30 +0300

    Change db pass to non-private

--- api/config/Database.php
+++ api/config/Database.php
@@ -4,7 +4,7 @@
         private $host = 'eu-cdbr-west-01.cleardb.com';
         private $db_name = 'heroku_6040d655babebd2';
         private $username = 'b7dc8d243d7a28';
-        private $password = env('DBPASS');
+        $password = env('DBPASS');
         private $conn;
 
         //DB Connect
