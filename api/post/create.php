46eff4a630e4605eecdfb28a265f75d47fe12d3d
Elen Liivapuu <eliivapuu@gmail.com>
2021-10-04 18:42:11 +0300

    Fix all paths

--- api/post/create.php
+++ api/post/create.php
@@ -3,8 +3,8 @@
     // header('Access-Control-Allow-Origin: *');
     // header('Content-Type: application/json');
 
-    require_once __DIR__.'/../../config/Database.php';
-    require_once __DIR__.'/../../models/Item.php';
+    include_once('../config/Database.php');
+    include_once('../models/Item.php');
 
     //Instantiate DB & connect
     $database_ = new Database();
