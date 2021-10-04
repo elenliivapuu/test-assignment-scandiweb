13561e42619c3a153e194d22b254037ca6d1cf17
Elen Liivapuu <eliivapuu@gmail.com>
2021-10-04 18:51:00 +0300

    Use password in plaintext for db

--- api/config/Database.php
+++ api/config/Database.php
@@ -4,7 +4,7 @@
         private $host = 'eu-cdbr-west-01.cleardb.com';
         private $db_name = 'heroku_6040d655babebd2';
         private $username = 'b7dc8d243d7a28';
-        private $password = $_ENV['DBPASS'];
+        private $password = 'c8cb6b6e';
         private $conn;
 
         //DB Connect
