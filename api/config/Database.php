f36fa8b7014e49c942cc4dee748d2d54110673e1
Elen Liivapuu <eliivapuu@gmail.com>
2021-10-04 18:49:18 +0300

    Use ENV for pass

--- api/config/Database.php
+++ api/config/Database.php
@@ -4,7 +4,7 @@
         private $host = 'eu-cdbr-west-01.cleardb.com';
         private $db_name = 'heroku_6040d655babebd2';
         private $username = 'b7dc8d243d7a28';
-        $password = env('DBPASS');
+        private $password = $_ENV['DBPASS'];
         private $conn;
 
         //DB Connect
